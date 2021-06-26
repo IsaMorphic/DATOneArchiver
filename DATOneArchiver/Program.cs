ddbb13e091d8bc4c663baa4e43471f9df7341bd7
Yodadude2003 <yodadude2003@gmail.com>
2021-06-26 17:22:41 -0500

    Add support for testing different endiannesses

--- DATOneArchiver/Program.cs
+++ DATOneArchiver/Program.cs
@@ -1,16 +1,23 @@
 ﻿using QuesoStruct;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace DATOneArchiver
 {
     class Program
     {
+        private static readonly Dictionary<string, Endianess> endianesses =
+            new Dictionary<string, Endianess> 
+            { 
+                { "LITTLE", Endianess.Little },
+                { "BIG", Endianess.Big },
+            };
         static void Main(string[] args)
         {
             using var stream = File.OpenRead(args[0]);
 
-            var archive = new Archive(stream, Endianess.Little);
+            var archive = new Archive(stream, endianesses[args[1].ToUpperInvariant()]);
             archive.Read();
 
             foreach (var file in archive.Files) 
